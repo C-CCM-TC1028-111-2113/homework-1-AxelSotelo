075a04635565295126574622ab930ea0161b4751
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-17 17:52:03 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -2,6 +2,17 @@ def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
     pass
+print("Give me an X coordinate")
+x1=float(input())
+print("Give me a Y coordinate")
+y1=float(input())
+print("Give me another X coordinate")
+x2=float(input())
+print("Give me another Y coordinate")
+y2=float(input())
+s= (y2-y1)/(x2-x1)
+print("the slope of the coordinates is", s)
+
 
 
 if __name__ == '__main__':
