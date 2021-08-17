96b235a8c85341e1e085c6b1ff95735271c67da7
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-17 17:35:23 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -2,5 +2,13 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+
+
+print("How many minutes did the snail ran?")
+m= float(input())
+cm = m * ((5.7*0.1)*60)
+
+print("The snail traveled ", cm, "cm")
 if __name__ == '__main__':
     main()
+   
