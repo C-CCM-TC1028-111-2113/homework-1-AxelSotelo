05f866e8de9c83666ac5d728a425b88fc006625f
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-18 18:16:41 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -2,12 +2,12 @@ def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
 
-    x1=float(input("Dame x1: ", ))
-    y1=float(input("Dame y1: ", ))
-    x2=float(input("Dame x2: ", ))
-    y2=float(input("Dame y2: ", ))
+    x1=float(input("Dame x1:", ))
+    y1=float(input("Dame y1:", ))
+    x2=float(input("Dame x2:", ))
+    y2=float(input("Dame y2:", ))
     s= (y2-y1)/(x2-x1)
-    print("Pendiente: ", s)
+    print("Pendiente:", s)
 
 
 if __name__ == '__main__':
