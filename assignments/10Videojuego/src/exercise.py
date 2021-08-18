142111fb4813daf4adb0ab328b5b6c71ee0aed63
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-18 16:42:36 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-new=int(input("Dame la cantidad de juegos nuevos "))
-used=int(input("Dame la cantidad de juegos usados "))
-tot= (new*1000)+(used*350)
-print("El total de la compra es", tot)
+
+    new=int(input("Dame la cantidad de juegos nuevos "))
+    used=int(input("Dame la cantidad de juegos usados "))
+    tot= (new*1000)+(used*350)
+    print("El total de la compra es", tot)
+    
 if __name__ == '__main__':
     main()
