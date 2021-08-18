82480d726ec92b5cf1168596dfb55908b84ea1a9
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-18 18:23:19 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,12 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
+    import math
 
-    x1=float(input("Dame x1: "))
-    y1=float(input("Dame y1: "))
-    x2=float(input("Dame x2: "))
-    y2=float(input("Dame y2: "))
-    s= (y2-y1)/(x2-x1)
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    s = (y2-y1)/(x2-x1)
+    
     print("Pendiente:", s)
 
 
