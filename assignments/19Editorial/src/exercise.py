8a9726c7fb8aca5fff5d501834cba606d867c677
AxelSotelo <88851084+AxelSotelo@users.noreply.github.com>
2021-08-18 16:49:51 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,13 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
 
+    wds= int(input("Dame el número de palabras "))
+    cst=float((wds/475))
+    pag= math.ceil(cst)
+    csto= pag*60
+    csds=csto-(csto*.1)
+    print("El costo de la publicación es ", csds)
 
 if __name__ == '__main__':
     main()
